5ee21f84588991395d709877992cfb960fc9c283
s3827202 <s3827202@rmit.student.edu.au>
2021-01-19 22:57:27 +1100

    Add project files.

---
+++ s3827202_s3687609_a2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace s3827202_s3687609_a2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
