7cc167623bbea3108ecce4654e7cb4860c8790cd
s3827202 <s3827202@rmit.student.edu.au>
2021-01-25 02:13:58 +1100

    Customer index added

---
+++ s3827202_s3687609_a2/Controllers/Customer.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using s3827202_s3687609_a2.Data;
+using s3827202_s3687609_a2.Models;
+using s3827202_s3687609_a2.Utilities;
+using s3827202_s3687609_a2.Filters;
+
+namespace s3827202_s3687609_a2.Controllers
+{
+    [AuthorizeCustomer]
+    public class CustomerController : Controller
+    {
+        private readonly BankDBContext _context;
+
+        // ReSharper disable once PossibleInvalidOperationException
+        private int CustomerID => HttpContext.Session.GetInt32(nameof(Customer.CustomerID)).Value;
+
+        public CustomerController(BankDBContext context) => _context = context;
+
+        public async Task<IActionResult> Index()
+        {
+            // Lazy loading.
+            // The Customer.Accounts property will be lazy loaded upon demand.
+            var customer = await _context.Customer.FindAsync(CustomerID);
+
+            // OR
+            // Eager loading.
+            //var customer = await _context.Customers.Include(x => x.Accounts).
+            //    FirstOrDefaultAsync(x => x.CustomerID == _customerID);
+
+            return View(customer);
+        }
+
+        public async Task<IActionResult> Deposit(int id) => View(await _context.Account.FindAsync(id));
+
+        [HttpPost]
+        public async Task<IActionResult> Deposit(int id, decimal amount)
+        {
+            var account = await _context.Account.FindAsync(id);
+
+            if (amount <= 0)
+                ModelState.AddModelError(nameof(amount), "Amount must be positive.");
+            if (amount.HasMoreThanTwoDecimalPlaces())
+                ModelState.AddModelError(nameof(amount), "Amount cannot have more than 2 decimal places.");
+            if (!ModelState.IsValid)
+            {
+                ViewBag.Amount = amount;
+                return View(account);
+            }
+
+            // Note this code could be moved out of the controller, e.g., into the Model.
+            account.Balance += amount;
+            account.Transactions.Add(
+                new Transaction
+                {
+                    TransactionType = TransactionType.Deposit,
+                    Amount = amount,
+                    ModifyDate = DateTime.UtcNow
+                });
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ s3827202_s3687609_a2/Utilities/MiscellaneousExtensionUtilities.cs
@@ -0,0 +1,8 @@
+﻿namespace s3827202_s3687609_a2.Utilities
+{
+    public static class MiscellaneousExtensionUtilities
+    {
+        public static bool HasMoreThanNDecimalPlaces(this decimal value, int n) => decimal.Round(value, n) != value;
+        public static bool HasMoreThanTwoDecimalPlaces(this decimal value) => value.HasMoreThanNDecimalPlaces(2);
+    }
+}
