1667f60dffeaa3e0635cdf6afca82e6e0c6e7f23
s3827202 <s3827202@rmit.student.edu.au>
2021-01-28 00:32:01 +1100

    setup

---
+++ McbaAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace McbaAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
